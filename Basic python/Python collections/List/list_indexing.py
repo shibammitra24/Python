5b93d0170220b4deb4549c0c79085e0ea6412a05
Shibam Mitra <shibammitra695@gmail.com>
2023-10-03 00:11:26 +0530

    Create list_indexing.py

---
+++ Basic python/Python collections/List/list_indexing.py
@@ -0,0 +1,6 @@
+lst = [12, 84, 24, 15, 37]
+
+# we can print a certain range within a list:
+print(lst)
+print(lst[1:4]) # staring index is inclusive and ending is exclusive
+print(lst[-3:-1]) # we can also do negative indexing
