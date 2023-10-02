08d23e14a6d57c828e926a94246a0c95fd3f1f8d
Shibam Mitra <shibammitra695@gmail.com>
2023-10-02 21:00:52 +0530

    Update while_loop.py

--- Basic python/Conditionals/while_loop.py
+++ Basic python/Conditionals/while_loop.py
@@ -1,4 +1,20 @@
-i = 0
-while i<=5:
-    print("Hello World!")
-    i=i+1
+# while loop is used to run a loop until the condition is true
+# i = 1
+# while i<=5:
+#     print("Hello World!", i)
+#     i=i+1
+
+# we can use the break keyword to stop the loop in a certain condition
+# j = 1
+# while j<=5:
+#     print(j)
+#     if j==3:
+#         break
+#     j = j + 1
+
+k=0
+while k<5:
+    k += 1
+    if k==3:
+        continue
+    print(k)
