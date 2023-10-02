29755f32b1d675d835313c33c69e4fa136787e31
Shibam Mitra <shibammitra695@gmail.com>
2023-10-03 00:11:39 +0530

    Create lists.py

---
+++ Basic python/Python collections/List/lists.py
@@ -0,0 +1,16 @@
+lst = [21, 68, 65, 87]
+print(lst) # print a list
+print(type(lst)) # check type of list
+print(len(lst)) # print length of list
+
+# check if an item is present in the list:
+print(87 in lst)
+# using if statement:
+if 87 in lst:
+    print("Item is present in list")
+
+# check if an item is not present in the list:
+print(55 not in lst)
+# using if statement:
+if 55 not in lst:
+    print("Item not present in list")
