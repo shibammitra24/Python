1fe4fcd3a6c3546fba86ec4437919530900ec9f8
Shibam Mitra <shibammitra695@gmail.com>
2023-10-03 23:23:44 +0530

    Create sort_list.py

---
+++ Basic python/Python collections/List/sort_list.py
@@ -0,0 +1,31 @@
+# we can sort a list 
+
+# sorting an int list
+a = [80, 50, 90, 10, 30]
+print(a)
+a.sort()
+print(a)
+
+# sorting an str list
+b = ["c", "d", "a", "b"]
+b1 = ["cat", "dog", "apple", "ball"]
+print(b)
+print(b1)
+b.sort()
+b1.sort()
+print(b)
+print(b1)
+
+# sort a list in reverse (descending way) 
+
+# sort a int list
+c = [30, 10, 90, 40, 70]
+print(c)
+c.sort(reverse=True)
+print(c)
+
+# sort a str list
+c1 = ["b", "a", "d", "c"]
+print(c1)
+c1.sort(reverse=True)
+print(c1)
