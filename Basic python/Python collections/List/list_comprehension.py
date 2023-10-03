d2acf750020f91195c92f7939b661a5ab7ccb479
Shibam Mitra <shibammitra695@gmail.com>
2023-10-03 23:47:33 +0530

    Update list_comprehension.py

--- Basic python/Python collections/List/list_comprehension.py
+++ Basic python/Python collections/List/list_comprehension.py
@@ -1,1 +1,15 @@
-    
+# list comprehension in python
+
+# using for loop:
+lst = [14, 58, 61, 21, 11]
+# new_lst = []
+
+# for i in lst:
+#     if i>20:
+#         new_lst.append(i)
+
+# print(new_lst)
+
+# using list comprehension:
+new_lst = [i for i in lst if i>20]
+print(new_lst)
